a8678cbee0eddffafc3caed06d8c9900d8169081
Joe Hohertz <jhohertz@gmail.com>
2017-01-19 11:28:35 -0500

    default java to 8u121, latest as of this commit

--- attributes/default.rb
+++ attributes/default.rb
@@ -76,3 +76,11 @@ default['rsyslog']['server']                    = true
 default['rsyslog']['protocol']                  = 'udp'
 default['rsyslog']['bind']                      = '127.0.0.1'
 
+
+default['java']['install_flavor']               = 'oracle'
+default['java']['jdk_version']                  = '8'
+default['java']['oracle']['accept_oracle_download_terms'] = true
+
+default['java']['jdk']['8']['x86_64']['url'] = 'http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-x64.tar.gz'
+default['java']['jdk']['8']['x86_64']['checksum'] = '97e30203f1aef324a07c94d9d078f5d19bb6c50e638e4492722debca588210bc'
+
